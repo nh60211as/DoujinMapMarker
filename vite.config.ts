1f0527195f8359c2ae6ab9e5e3c9cf68eb3467a2
nh60211as <nh60211@gmail.com>
2024-12-29 17:44:46 +0800

    build: add route prefix 5

--- vite.config.ts
+++ vite.config.ts
@@ -1,56 +1,58 @@
-import preact from '@preact/preset-vite';
-import { defineConfig, loadEnv } from 'vite';
+import preact from "@preact/preset-vite";
+import { defineConfig, loadEnv } from "vite";
 import checker from "vite-plugin-checker";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-	const env = loadEnv(mode, process.cwd(), "");
-	
-	return {plugins: [
-		preact({
-			prerender: {
-				enabled: true,
-				renderTarget: '#app',
-				additionalPrerenderRoutes: ['/404'],
-				previewMiddlewareEnabled: true,
-				previewMiddlewareFallback: '/404',
-			},
-		}),
-		checker({
-		  typescript: true,
-		}),
-		VitePWA({
-		  manifest: {
-			name: "Fancy Frontier Map Marker",
-			short_name: "FF Map Marker",
-			start_url: "/FfMapMarker",
-			display: "standalone",
-			orientation: "portrait",
-			background_color: "#000000",
-			theme_color: "#000000",
-			icons: [
-			  {
-				src: "android-chrome-192x192.png",
-				type: "image/png",
-				sizes: "192x192",
-			  },
-			  {
-				src: "android-chrome-512x512.png",
-				type: "image/png",
-				sizes: "512x512",
-			  },
-			],
-		  },
-		  workbox: { cleanupOutdatedCaches: true },
-		}),
-	],
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    base: "/FfMapMarker/",
+    plugins: [
+      preact({
+        prerender: {
+          enabled: true,
+          renderTarget: "#app",
+          additionalPrerenderRoutes: ["/404"],
+          previewMiddlewareEnabled: true,
+          previewMiddlewareFallback: "/404",
+        },
+      }),
+      checker({
+        typescript: true,
+      }),
+      VitePWA({
+        manifest: {
+          name: "Fancy Frontier Map Marker",
+          short_name: "FF Map Marker",
+          start_url: "/FfMapMarker/",
+          display: "standalone",
+          orientation: "portrait",
+          background_color: "#000000",
+          theme_color: "#000000",
+          icons: [
+            {
+              src: "android-chrome-192x192.png",
+              type: "image/png",
+              sizes: "192x192",
+            },
+            {
+              src: "android-chrome-512x512.png",
+              type: "image/png",
+              sizes: "512x512",
+            },
+          ],
+        },
+        workbox: { cleanupOutdatedCaches: true },
+      }),
+    ],
     build: {
       outDir: "./build",
     },
-	define: {
-		// genius design
-		"import.meta.env.ROUTE_PREFIX": JSON.stringify(env.ROUTE_PREFIX),
-	  },
-	}
+    define: {
+      // genius design
+      "import.meta.env.ROUTE_PREFIX": JSON.stringify(env.ROUTE_PREFIX),
+    },
+  };
 });
