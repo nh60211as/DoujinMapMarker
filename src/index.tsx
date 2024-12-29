b2521bc104643c905c3b26b1845ac4f7e8555859
nh60211as <nh60211@gmail.com>
2024-12-29 18:11:10 +0800

    build: add route prefix 8

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,6 @@ import { Home } from "./pages/Home/index.jsx";
 import { NotFound } from "./pages/_404.jsx";
 import { Header } from "./components/Header.js";
 import "./index.css";
-import { ROUTE_PREFIX } from "./Config.js";
 import { render } from "preact";
 
 export function App() {
@@ -17,7 +16,7 @@ export function App() {
       <Header />
       <main>
         <Router>
-          <Route path={`/${ROUTE_PREFIX}`} component={Home} />
+          <Route path={`/`} component={Home} />
           <Route default component={NotFound} />
         </Router>
       </main>
