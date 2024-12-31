49f4b25b77c573fc0e81fb2061933ebd746d963e
nh60211as <nh60211@gmail.com>
2024-12-31 18:28:47 +0800

    feat: separate Ff44MapData by row

---
+++ src/data/Ff44MapData_S.ts
@@ -0,0 +1,33 @@
+import { BoothActiveDay, MapData } from "../types/MapData";
+
+export const Ff44MapData_S: Array<MapData> = [
+  // Day 1
+  {
+    id: "32014cff102c4d70a654d6b02d3e7276",
+    dimension: {
+      x: 1609,
+      y: 1080,
+      width: 41,
+      height: 51,
+    },
+    boothName: "台北人",
+    boothNumbers: ["S03", "S04"],
+    boothActiveDays: [BoothActiveDay.day1],
+    boothLink: undefined,
+  },
+  // Day 2
+  {
+    id: "2c2879ac43364e738e9dd3737d1232b0",
+    dimension: {
+      x: 1566,
+      y: 760,
+      width: 41,
+      height: 25,
+    },
+    boothName: "台北人",
+    boothNumbers: ["S33"],
+    boothActiveDays: [BoothActiveDay.day2],
+    boothLink: undefined,
+  },
+  // Day 3
+];
---
+++ src/data/Ff44MapData_T.ts
@@ -0,0 +1,24 @@
+import { BoothActiveDay, MapData } from "../types/MapData";
+
+export const Ff44MapData_T: Array<MapData> = [
+  // Day 1
+  {
+    id: "c8c7be762d444bb8b8f3b14bedda760a",
+    dimension: {
+      x: 1381,
+      y: 548,
+      width: 41,
+      height: 51,
+    },
+    boothName: "壞菇社",
+    boothNumbers: ["T25", "T26"],
+    boothActiveDays: [
+      BoothActiveDay.day1,
+      BoothActiveDay.day2,
+      BoothActiveDay.day3,
+    ],
+    boothLink: "https://www.facebook.com/MARK4why",
+  },
+  // Day 2
+  // Day 3
+];
