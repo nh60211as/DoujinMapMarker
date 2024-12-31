1da805174d2945ecb3bd33621e10dc34ac68bc0e
nh60211as <nh60211@gmail.com>
2024-12-31 20:02:39 +0800

    feat: add Ff44MapData_B day 1

--- src/data/Ff44MapData.ts
+++ src/data/Ff44MapData.ts
@@ -1,9 +1,12 @@
 import { BoothActiveDay, MapData } from "../types/MapData";
 import { Ff44MapData_A } from "./Ff44MapData_A";
+import { Ff44MapData_B } from "./Ff44MapData_B";
 import { Ff44MapData_S } from "./Ff44MapData_S";
 import { Ff44MapData_T } from "./Ff44MapData_T";
 
-const Ff44MapData: Array<MapData> = Ff44MapData_A.concat(
+const Ff44MapData: Array<MapData> = ([] as Array<MapData>).concat(
+  Ff44MapData_A,
+  Ff44MapData_B,
   Ff44MapData_S,
   Ff44MapData_T,
 );
