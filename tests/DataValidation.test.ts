36f7ad7157438e5cbdd0d3fe68d7abb47c3e7b01
nh60211as <nh60211@gmail.com>
2025-01-01 19:18:39 +0800

    featL add Day 1 K Row

--- tests/DataValidation.test.ts
+++ tests/DataValidation.test.ts
@@ -3,23 +3,23 @@ import { FF44_GROUP_DATA } from "../src/data/Ff44MapData";
 
 describe("Validate FF 44 Map Data", () => {
   test("No Duplicate Group ID", () => {
-    const groupIdList = new Set<string>()
+    const groupIdList = new Set<string>();
 
     for (const groupData of FF44_GROUP_DATA) {
-        if (groupIdList.has(groupData.groupId)) {
-          assert.fail(`Duplicate group ID [${groupData.groupId}]`)
-        }
-        groupIdList.add(groupData.groupId);
+      if (groupIdList.has(groupData.groupId)) {
+        assert.fail(`Duplicate group ID [${groupData.groupId}]`);
       }
+      groupIdList.add(groupData.groupId);
+    }
   });
   test("No Duplicate Group Name", () => {
-    const groupNameList = new Set<string>()
+    const groupNameList = new Set<string>();
 
     for (const groupData of FF44_GROUP_DATA) {
-        if (groupNameList.has(groupData.groupName)) {
-          assert.fail(`Duplicate group name [${groupData.groupName}]`)
-        }
-        groupNameList.add(groupData.groupName);
+      if (groupNameList.has(groupData.groupName)) {
+        assert.fail(`Duplicate group name [${groupData.groupName}]`);
       }
+      groupNameList.add(groupData.groupName);
+    }
   });
 });
