9cb267d87157fbe99e2b5764db6f1f334b687c9f
nh60211as <nh60211@gmail.com>
2025-01-01 17:52:02 +0800

    featL add Day 1 F Row

--- src/data/Ff44MapData.ts
+++ src/data/Ff44MapData.ts
@@ -2081,6 +2081,486 @@ const FF44_GROUP_DATA: Array<GroupData> = [
     ],
   },
   {
+    groupId: "936bfbbddb0e46bc8d49b7fd22982201",
+    groupName: "毛毛精靈與牠們的喵喵迷因",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "F", number: 1 },
+          { row: "F", number: 2 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [
+          { row: "F", number: 1 },
+          { row: "F", number: 2 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [
+          { row: "F", number: 1 },
+          { row: "F", number: 2 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "52f873fa70264c06b0b0790c05b01f11",
+    groupName: "宅鼠-Nezumi",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "F", number: 3 }],
+      },
+    ],
+  },
+  {
+    groupId: "178ba38064e94852a151fb7bfb999db6",
+    groupName: "週五沒朋友",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "F", number: 4 }],
+      },
+    ],
+  },
+  {
+    groupId: "d3db6e46b3fc43cf8eb3f34c454df871",
+    groupName: "六本木製作所",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "F", number: 5 }],
+      },
+    ],
+  },
+  {
+    groupId: "af30f6be4fe1407f8a9323a87181cea4",
+    groupName: "山上風大",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "F", number: 6 }],
+      },
+    ],
+  },
+  {
+    groupId: "41665cdc7b7f4ef781bd9a654c217379",
+    groupName: "角落老師小夥伴",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "F", number: 7 }],
+      },
+    ],
+  },
+  {
+    groupId: "3bb703c57ea941ec800adc1d69a318a5",
+    groupName: "快樂貨櫃屋",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "F", number: 8 }],
+      },
+    ],
+  },
+  {
+    groupId: "48c095af54584d168b43745669318d4f",
+    groupName: "Lotai愛莉塔",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "F", number: 9 }],
+      },
+    ],
+  },
+  {
+    groupId: "ce9324effdc84c39b2a7bd40680a0763",
+    groupName: "塗鴉時空間",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "F", number: 10 }],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "N", number: 34 }],
+      },
+    ],
+  },
+  {
+    groupId: "ef475c9ebf5b4477a1aaf5ef6928625e",
+    groupName: "西窗夜雨",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "F", number: 11 },
+          { row: "F", number: 12 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [
+          { row: "N", number: 35 },
+          { row: "N", number: 36 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "5355d67f341e40a7bf5c0f31ca69af37",
+    groupName: "U235核燃動力",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "F", number: 13 }],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "N", number: 37 }],
+      },
+    ],
+  },
+  {
+    groupId: "0434145e5e4e4edf97a2744ff38daa04",
+    groupName: "盒子裡的猫咪",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "F", number: 14 }],
+      },
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "D", number: 16 }],
+      },
+    ],
+  },
+  {
+    groupId: "1b6b9fecccdd4f34835dfeb4d10f6ae2",
+    groupName: "我來、我見、我射爆",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "F", number: 15 },
+          { row: "F", number: 16 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "01d95071aeef495d99a056c7274fd153",
+    groupName: "晨光乳品製作所",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "F", number: 17 }],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "N", number: 30 }],
+      },
+    ],
+  },
+  {
+    groupId: "eb70499f229d4b71a5ee1e45a197a1b1",
+    groupName: "隻雞麵包坊",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "F", number: 18 }],
+      },
+    ],
+  },
+  {
+    groupId: "17e04da26b4640929a2c0505f8819649",
+    groupName: "瓜茶ice社",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "F", number: 19 },
+          { row: "F", number: 20 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [
+          { row: "E", number: 29 },
+          { row: "E", number: 30 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "67340e10cd0e4448b3fcd2eb97d77864",
+    groupName: "龜甲萬皮卡丘",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "F", number: 21 },
+          { row: "F", number: 22 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "0784568ab2e24a2d8b3eaa32cd28881c",
+    groupName: "吃餅進ジーン",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "F", number: 23 },
+          { row: "F", number: 24 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [
+          { row: "E", number: 43 },
+          { row: "E", number: 44 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "96548277ecd34265a558933688208fcf",
+    groupName: "肚臍轉圈圈",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "F", number: 25 },
+          { row: "F", number: 26 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [
+          { row: "E", number: 41 },
+          { row: "E", number: 42 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "296bc876d8c44721b329268dac9c2846",
+    groupName: "DAi",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "F", number: 27 },
+          { row: "F", number: 28 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "E", number: 33 }],
+      },
+    ],
+  },
+  {
+    groupId: "c55b1b8a62ea4c29885b7455f84584b4",
+    groupName: "南瓜男pumpkin man",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "F", number: 29 }],
+      },
+    ],
+  },
+  {
+    groupId: "b9ddcb9a05824a63a6293da2bb88b9cb",
+    groupName: "千仞蘭庭",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "F", number: 30 }],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "I", number: 37 }],
+      },
+    ],
+  },
+  {
+    groupId: "2370a29a1f184250b8bc742e08b2e126",
+    groupName: "左岸喵喵館",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "F", number: 31 }],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "F", number: 32 }],
+      },
+    ],
+  },
+  {
+    groupId: "524f4521e4914d6e8659ddfedcd0fd8d",
+    groupName: "頭上有犄角",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "F", number: 33 }],
+      },
+    ],
+  },
+  {
+    groupId: "a4b9a556cad8474f980b6c0e6fad8fc6",
+    groupName: "6歲被颱風吹走的『 羊 』?",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "F", number: 34 }],
+      },
+    ],
+  },
+  {
+    groupId: "a82a747e29ca4f14b943cea49889348e",
+    groupName: "黑貓白眉",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "F", number: 35 }],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "E", number: 22 }],
+      },
+    ],
+  },
+  {
+    groupId: "1a8028a86b0343b0b5466324a437263d",
+    groupName: "異私特",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "F", number: 36 }],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "E", number: 20 }],
+      },
+    ],
+  },
+  {
+    groupId: "d1d91d94e9d741f3ac7d7ef97c5afacb",
+    groupName: "貓兒迷雅",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "F", number: 37 },
+          { row: "F", number: 38 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [
+          { row: "F", number: 37 },
+          { row: "F", number: 38 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "b65b2f45f48749daaa127eaecef3ba8c",
+    groupName: "Omelet Cream",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "F", number: 39 },
+          { row: "F", number: 40 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [
+          { row: "F", number: 40 },
+          { row: "F", number: 41 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "331512ee8f2c409f9d9f8fc2e5ffc519",
+    groupName: "水餃寶包",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "F", number: 41 },
+          { row: "F", number: 42 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "F", number: 42 }],
+      },
+    ],
+  },
+  {
+    groupId: "84e67802f9b541839f50df0da06d5f31",
+    groupName: "捕食少女",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "F", number: 43 },
+          { row: "F", number: 44 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [
+          { row: "F", number: 43 },
+          { row: "F", number: 44 },
+        ],
+      },
+    ],
+  },
+  {
     groupId: "7d9a2f29855241cb8c7c435ea34020e2",
     groupName: "F.W.ZHolic",
     groupLink: "https://www.facebook.com/fwzholic",
