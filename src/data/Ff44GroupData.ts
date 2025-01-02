854768a9e865c98c229e7ae06367bfac5902eedd
nh60211as <nh60211@gmail.com>
2025-01-02 20:49:53 +0800

    feat: add Day 3 C Row

--- src/data/Ff44GroupData.ts
+++ src/data/Ff44GroupData.ts
@@ -49,6 +49,7 @@ import { FF44_GROUP_DATA_DAY_2_ROW_U } from "./ff44/day2/Ff44GroupDataDay2RowU";
 import { FF44_GROUP_DATA_DAY_2_ROW_W } from "./ff44/day2/Ff44GroupDataDay2RowW";
 import { FF44_GROUP_DATA_DAY_3_ROW_A } from "./ff44/day3/Ff44GroupDataDay3RowA";
 import { FF44_GROUP_DATA_DAY_3_ROW_B } from "./ff44/day3/Ff44GroupDataDay3RowB";
+import { FF44_GROUP_DATA_DAY_3_ROW_C } from "./ff44/day3/Ff44GroupDataDay3RowC";
 
 export const FF44_GROUP_DATA: Array<GroupData> = (
   [] as Array<GroupData>
@@ -99,6 +100,7 @@ export const FF44_GROUP_DATA: Array<GroupData> = (
   FF44_GROUP_DATA_DAY_2_ROW_W,
   FF44_GROUP_DATA_DAY_3_ROW_A,
   FF44_GROUP_DATA_DAY_3_ROW_B,
+  FF44_GROUP_DATA_DAY_3_ROW_C,
   [
     {
       groupId: "3a2aaafd0b7f410c88ddff6b9156b4ac",
---
+++ src/data/ff44/day3/Ff44GroupDataDay3RowC.ts
@@ -0,0 +1,290 @@
+import { BoothActiveDay } from "../../../types/BoothActiveDay";
+import { GroupData } from "../../../types/GroupData";
+
+export const FF44_GROUP_DATA_DAY_3_ROW_C: Array<GroupData> = [
+  {
+    groupId: "05488e1e367a41b89824c03d46e3fc18",
+    groupName: "MR工作室",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "C", number: 1 }],
+      },
+    ],
+  },
+  {
+    groupId: "982113aa5fea4810802685d69b648e37",
+    groupName: "貓罐販售所",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "C", number: 2 }],
+      },
+    ],
+  },
+  {
+    groupId: "500d3f03d1a24e17bc084f895983ca00",
+    groupName: "7MA",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "C", number: 4 }],
+      },
+    ],
+  },
+  {
+    groupId: "52a261659f0a4c21b739ca570e02eafa",
+    groupName: "Love SHOP",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [
+          { row: "C", number: 5 },
+          { row: "C", number: 6 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "13d141a721a54a5abd217949e93524da",
+    groupName: "雪兔紙",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [
+          { row: "C", number: 7 },
+          { row: "C", number: 8 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "95d4838516784f28a6d12cdaa70b4080",
+    groupName: "易爛",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "C", number: 9 }],
+      },
+    ],
+  },
+  {
+    groupId: "73c4e6d380394a738868db56de32d4bd",
+    groupName: "兩齒",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "C", number: 10 }],
+      },
+    ],
+  },
+  {
+    groupId: "8513d37d2944437fb094038b1ac9a251",
+    groupName: "MAkoriii",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "C", number: 13 }],
+      },
+    ],
+  },
+  {
+    groupId: "d819c77f9f8e45a2b682f8e88d07c93b",
+    groupName: "哈比不是哈比人",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [
+          { row: "C", number: 17 },
+          { row: "C", number: 18 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "aa5d2eebfe6745b2a1fc3de5e993436f",
+    groupName: "No day,no night",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "C", number: 20 }],
+      },
+    ],
+  },
+  {
+    groupId: "0f999d5c3116487eb8f7feda318c15d8",
+    groupName: "夜觸人身",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [
+          { row: "C", number: 23 },
+          { row: "C", number: 24 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "10c54091ca2a453c948db91c803fff97",
+    groupName: "STARWALKER STUDIO",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "C", number: 25 }],
+      },
+    ],
+  },
+  {
+    groupId: "6ffc4b1a1c6540019a14afa907c9247f",
+    groupName: "カルデアごはん!!",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "C", number: 26 }],
+      },
+    ],
+  },
+  {
+    groupId: "f26af2dd52b24dcfaa03703d41744eb3",
+    groupName: "Atelier Sizutsuki",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "C", number: 27 }],
+      },
+    ],
+  },
+  {
+    groupId: "400595f0808f46c1b01114cc901d79bd",
+    groupName: "不要碰我的狗",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "C", number: 28 }],
+      },
+    ],
+  },
+  {
+    groupId: "6b1b55077b5b428ba1f915556c03f9b2",
+    groupName: "R.I.N",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [
+          { row: "C", number: 29 },
+          { row: "C", number: 30 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "d773f9099f5d48ee80079961aac58259",
+    groupName: "白芍產糧舖",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "C", number: 31 }],
+      },
+    ],
+  },
+  {
+    groupId: "33ed6493cb2547e2ba8b292402b1cf47",
+    groupName: "Lycoris Garden",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "C", number: 32 }],
+      },
+    ],
+  },
+  {
+    groupId: "6e7446e205e94ca7912f10347633c030",
+    groupName: "荒野貼紙舖",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "C", number: 33 }],
+      },
+    ],
+  },
+  {
+    groupId: "38c28ded8d3a454d8118a17e41652471",
+    groupName: "AIZAWA★藍沢津",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "C", number: 34 }],
+      },
+    ],
+  },
+  {
+    groupId: "114620b4ccc24e4294bfa8d3585be6b9",
+    groupName: "飛翔吧！埼玉湾連合",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [
+          { row: "C", number: 35 },
+          { row: "C", number: 36 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "408c0be6afd543fe886bd77a953ecbe4",
+    groupName: "Moe Linkage",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [
+          { row: "C", number: 37 },
+          { row: "C", number: 38 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "538833c7ad744756bba1db9921aef494",
+    groupName: "真央城公園",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "C", number: 40 }],
+      },
+    ],
+  },
+  {
+    groupId: "5cfee6904868431d9ff823be9ef541ad",
+    groupName: "木木梟保育協會",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "C", number: 44 }],
+      },
+    ],
+  },
+];
