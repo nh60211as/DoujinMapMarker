ad03d2902418afe443ce31a8271c0dd5ee4df328
nh60211as <nh60211@gmail.com>
2025-01-02 10:19:11 +0800

    featL add Day 1 Q Row

---
+++ src/data/ff44/day1/Ff44GroupDataDay1RowQ.ts
@@ -0,0 +1,496 @@
+import { BoothActiveDay } from "../../../types/BoothActiveDay";
+import { GroupData } from "../../../types/GroupData";
+
+export const FF44_GROUP_DATA_DAY_1_ROW_Q: Array<GroupData> = [
+  {
+    groupId: "9ba1f1ac522d46fcb2b51c5a26a56c08",
+    groupName: "Mon夢",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "Q", number: 1 },
+          { row: "Q", number: 2 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [
+          { row: "Q", number: 1 },
+          { row: "Q", number: 2 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "64dd8b84b0c34f2d8f7529d5cb8ea6dc",
+    groupName: "星喵Aastaneko",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "Q", number: 3 },
+          { row: "Q", number: 4 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "3f9406d3d95a412e84c70ac6414243a8",
+    groupName: "安安嘟嘟好",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "Q", number: 5 }],
+      },
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "Q", number: 3 }],
+      },
+    ],
+  },
+  {
+    groupId: "5043eeab54354d0a8f0baae7a07df3ec",
+    groupName: "鈴鈴的水屬性媽媽",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "Q", number: 6 }],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "Q", number: 6 }],
+      },
+    ],
+  },
+  {
+    groupId: "f5af6a9b167647429223d69318b947c2",
+    groupName: "泥泥汝的魔法學院",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "Q", number: 7 },
+          { row: "Q", number: 8 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "Q", number: 3 }],
+      },
+    ],
+  },
+  {
+    groupId: "e130ac8dd43f4264ae87c391fe30fd28",
+    groupName: "ShiragaYanko小泱",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "Q", number: 9 },
+          { row: "Q", number: 10 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "1c0a0d298a0f4641aac2649fc311d93d",
+    groupName: "小小小萌雪",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "Q", number: 11 },
+          { row: "Q", number: 12 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "Q", number: 11 }],
+      },
+    ],
+  },
+  {
+    groupId: "a142d54e6dcc4dbcbc8197dc4170ed30",
+    groupName: "星野秀奈★XUXU",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "Q", number: 13 },
+          { row: "Q", number: 14 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "Q", number: 14 }],
+      },
+    ],
+  },
+  {
+    groupId: "bffae85f5a3a40afbf8eb5f69fd92653",
+    groupName: "咖啡 cosplay",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "Q", number: 15 },
+          { row: "Q", number: 16 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "Q", number: 16 }],
+      },
+    ],
+  },
+  {
+    groupId: "46ab9eae8da8457f8b079641bdb3aa5d",
+    groupName: "-Stityo悠&Liya璃犽-",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "Q", number: 17 },
+          { row: "Q", number: 18 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "Q", number: 17 }],
+      },
+    ],
+  },
+  {
+    groupId: "daa21aff4b304de38d28d3f361c67fd7",
+    groupName: "Cosis - Jooa",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "Q", number: 19 },
+          { row: "Q", number: 20 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "0e0b0ec88ee9477f8ae11ded6f327188",
+    groupName: "Cosis-Berry",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "Q", number: 21 },
+          { row: "Q", number: 22 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "Q", number: 22 }],
+      },
+    ],
+  },
+  {
+    groupId: "4f82621ce57245cb9333c2d1d3efdbd8",
+    groupName: "JasperZ x Meenfox",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "Q", number: 23 },
+          { row: "Q", number: 24 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [
+          { row: "Q", number: 23 },
+          { row: "Q", number: 24 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "65243bf04fe44ad69c495ddf322c29c8",
+    groupName: "MIYANA咪呀",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "Q", number: 25 },
+          { row: "Q", number: 26 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "R", number: 33 }],
+      },
+    ],
+  },
+  {
+    groupId: "a010c92b52af45a79c4add9e428fbc72",
+    groupName: "sugar melt",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "Q", number: 27 },
+          { row: "Q", number: 28 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [
+          { row: "Q", number: 27 },
+          { row: "Q", number: 28 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "837e2e85de83485aa7195e38beaefd44",
+    groupName: "紅葉 Momiko",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "Q", number: 29 },
+          { row: "Q", number: 30 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [
+          { row: "Q", number: 29 },
+          { row: "Q", number: 30 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "aaf4f8b9cebd486e8b6928f9807d676a",
+    groupName: "林檎的貓主子",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "Q", number: 31 }],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "Q", number: 31 }],
+      },
+    ],
+  },
+  {
+    groupId: "74293d3855c645418e2213f4625fb51f",
+    groupName: "deer62",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "Q", number: 32 }],
+      },
+    ],
+  },
+  {
+    groupId: "b6838989ca3e49c980ed104bec74f113",
+    groupName: "Yuiii教✝️",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "Q", number: 33 }],
+      },
+    ],
+  },
+  {
+    groupId: "337d1a8740f340aea8f0800170d099ab",
+    groupName: "梓瞳X晨晨",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "Q", number: 34 }],
+      },
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "R", number: 18 }],
+      },
+    ],
+  },
+  {
+    groupId: "90f61a5a4c7d4943aea23b630860f443",
+    groupName: "小夜魔法學院",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "Q", number: 35 },
+          { row: "Q", number: 36 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "Q", number: 37 }],
+      },
+    ],
+  },
+  {
+    groupId: "47298edc02784924af07c85cd4a64415",
+    groupName: "YJ Book Studio",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "Q", number: 37 }],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "Q", number: 37 }],
+      },
+    ],
+  },
+  {
+    groupId: "c67646a907b8494aae9124493845dce4",
+    groupName: "天使堡佑",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "Q", number: 38 }],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "Q", number: 38 }],
+      },
+    ],
+  },
+  {
+    groupId: "3afd2a4e01de45d7ba4823ae2fe227e6",
+    groupName: "紳士們的聚集",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "Q", number: 39 }],
+      },
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "Q", number: 39 }],
+      },
+    ],
+  },
+  {
+    groupId: "158234be6e1e422da09cc8eec2253485",
+    groupName: "野人文庫",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "Q", number: 40 }],
+      },
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "Q", number: 40 }],
+      },
+    ],
+  },
+  {
+    groupId: "bf155ecd0f604bc3b04a2ee14b6dab1c",
+    groupName: "犬神Inugami",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "Q", number: 41 }],
+      },
+    ],
+  },
+  {
+    groupId: "ec5cb39e1a2e4244bc156e70ff10efa2",
+    groupName: "龜龜kame",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [{ row: "Q", number: 42 }],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "Q", number: 42 }],
+      },
+    ],
+  },
+  {
+    groupId: "9a88dbc35c85437aaf5a0fd383921b7d",
+    groupName: "伶伶Linnnng",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "Q", number: 43 },
+          { row: "Q", number: 44 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "Q", number: 44 }],
+      },
+    ],
+  },
+  {
+    groupId: "cfb246471d274008821c8a8af05642bd",
+    groupName: "小律 Ritsu與她的火火豬",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "Q", number: 45 },
+          { row: "Q", number: 46 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [
+          { row: "Q", number: 45 },
+          { row: "Q", number: 46 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "978aec1ee4514dcb81cb45ba86371b31",
+    groupName: "Tsuru鶴葵",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day1,
+        boothNumberList: [
+          { row: "Q", number: 47 },
+          { row: "Q", number: 48 },
+        ],
+      },
+    ],
+  },
+];
