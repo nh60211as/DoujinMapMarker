e9f5e3a713c5b4f5b3e96680d63d8400a8ae6bbf
nh60211as <nh60211@gmail.com>
2025-01-02 21:15:06 +0800

    feat: add Day 3 F Row

--- src/data/Ff44GroupData.ts
+++ src/data/Ff44GroupData.ts
@@ -52,6 +52,7 @@ import { FF44_GROUP_DATA_DAY_3_ROW_B } from "./ff44/day3/Ff44GroupDataDay3RowB";
 import { FF44_GROUP_DATA_DAY_3_ROW_C } from "./ff44/day3/Ff44GroupDataDay3RowC";
 import { FF44_GROUP_DATA_DAY_3_ROW_D } from "./ff44/day3/Ff44GroupDataDay3RowD";
 import { FF44_GROUP_DATA_DAY_3_ROW_E } from "./ff44/day3/Ff44GroupDataDay3RowE";
+import { FF44_GROUP_DATA_DAY_3_ROW_F } from "./ff44/day3/Ff44GroupDataDay3RowF";
 
 export const FF44_GROUP_DATA: Array<GroupData> = (
   [] as Array<GroupData>
@@ -105,6 +106,7 @@ export const FF44_GROUP_DATA: Array<GroupData> = (
   FF44_GROUP_DATA_DAY_3_ROW_C,
   FF44_GROUP_DATA_DAY_3_ROW_D,
   FF44_GROUP_DATA_DAY_3_ROW_E,
+  FF44_GROUP_DATA_DAY_3_ROW_F,
   [
     {
       groupId: "3a2aaafd0b7f410c88ddff6b9156b4ac",
---
+++ src/data/ff44/day3/Ff44GroupDataDay3RowF.ts
@@ -0,0 +1,93 @@
+import { BoothActiveDay } from "../../../types/BoothActiveDay";
+import { GroupData } from "../../../types/GroupData";
+
+export const FF44_GROUP_DATA_DAY_3_ROW_F: Array<GroupData> = [
+  {
+    groupId: "74c5edccb17243c2b6d5136b09ac3fa8",
+    groupName: "水水的水世界",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "F", number: 7 }],
+      },
+    ],
+  },
+  {
+    groupId: "4aac6475d2e546e890125e1e2bc2692a",
+    groupName: "極限邊緣",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "F", number: 8 }],
+      },
+    ],
+  },
+  {
+    groupId: "25f2c92402cc461086683ae78ac70162",
+    groupName: "SOLID AIR (台湾出張)",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "F", number: 11 }],
+      },
+    ],
+  },
+  {
+    groupId: "07119dd41fb24a079cb63b7dcae79e5c",
+    groupName: "雨喵yumeow",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "F", number: 15 }],
+      },
+    ],
+  },
+  {
+    groupId: "77fce2be907c424caa4cf3afca0ea7e8",
+    groupName: "k鴻畫不完",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "F", number: 16 }],
+      },
+    ],
+  },
+  {
+    groupId: "9197ce7653c14296b3ece175b888912d",
+    groupName: "小卡俄的永夜國度",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "F", number: 17 }],
+      },
+    ],
+  },
+  {
+    groupId: "07cb793f646440a680ed98b7441e1364",
+    groupName: "阿熊人熊肉鍋",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "F", number: 18 }],
+      },
+    ],
+  },
+  {
+    groupId: "06d92e777e764e359218e7fec69f267a",
+    groupName: "微光創造",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "F", number: 40 }],
+      },
+    ],
+  },
+];
