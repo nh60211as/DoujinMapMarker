800f8891909f266b5c8ec1ca8b15fbf631575843
nh60211as <nh60211@gmail.com>
2025-01-02 14:06:02 +0800

    featL add Day 2 D Row

---
+++ src/data/ff44/day2/Ff44GroupDataDay2RowD.ts
@@ -0,0 +1,215 @@
+import { BoothActiveDay } from "../../../types/BoothActiveDay";
+import { GroupData } from "../../../types/GroupData";
+
+export const FF44_GROUP_DATA_DAY_2_ROW_D: Array<GroupData> = [
+  {
+    groupId: "8c4ccc432aff4ae18328d9a2f99aeca0",
+    groupName: "彩音 〜xi-on〜",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [
+          { row: "D", number: 5 },
+          { row: "D", number: 6 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [
+          { row: "D", number: 3 },
+          { row: "D", number: 4 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "8a0c16c3c6a848c68eefe4208e99e486",
+    groupName: "Yonder Voice",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "D", number: 7 }],
+      },
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "D", number: 5 }],
+      },
+    ],
+  },
+  {
+    groupId: "da8b7323b96e432893bccfc18eb83c93",
+    groupName: "江江優夜",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "D", number: 8 }],
+      },
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "D", number: 11 }],
+      },
+    ],
+  },
+  {
+    groupId: "90572fd0235c4d3eab36863fba1147f7",
+    groupName: "洩矢米米米",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "D", number: 9 }],
+      },
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "D", number: 9 }],
+      },
+    ],
+  },
+  {
+    groupId: "77631cf60f954e5797118e67336d5fd1",
+    groupName: "夜大大工作室",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "D", number: 10 }],
+      },
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "D", number: 10 }],
+      },
+    ],
+  },
+  {
+    groupId: "18fec22b6c7a4e70a770353628c8e2c8",
+    groupName: "東方project文文新聞資訊部",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "D", number: 12 }],
+      },
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "D", number: 12 }],
+      },
+    ],
+  },
+  {
+    groupId: "58a6f636334847adb5e1380deb6b81d7",
+    groupName: "讓我再多睡一會",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "D", number: 13 }],
+      },
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "D", number: 13 }],
+      },
+    ],
+  },
+  {
+    groupId: "60db71c70af8403d9033b10efe29104f",
+    groupName: "taimini棉花娃衣",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [{ row: "D", number: 26 }],
+      },
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "D", number: 32 }],
+      },
+    ],
+  },
+  {
+    groupId: "c21f61ca116e47f4a7a49665c319d8da",
+    groupName: "ZAP製造所",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [
+          { row: "D", number: 27 },
+          { row: "D", number: 28 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [
+          { row: "D", number: 33 },
+          { row: "D", number: 34 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "50627b55cdc049808512a2d4d62d2335",
+    groupName: "鋁飾布裳兔",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [
+          { row: "D", number: 29 },
+          { row: "D", number: 30 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [
+          { row: "D", number: 29 },
+          { row: "D", number: 30 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "04f7b2ee2c884154b1a1921c427a233c",
+    groupName: "遇見幸福手創館 Met Happiness",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [
+          { row: "D", number: 41 },
+          { row: "D", number: 42 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [
+          { row: "D", number: 41 },
+          { row: "D", number: 42 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "379777cd1980477ca5fbfe8fbdec492e",
+    groupName: "日落之旅藥水舖",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day2,
+        boothNumberList: [
+          { row: "D", number: 43 },
+          { row: "D", number: 44 },
+        ],
+      },
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [
+          { row: "D", number: 43 },
+          { row: "D", number: 44 },
+        ],
+      },
+    ],
+  },
+];
