de1d78df1ef4ed281bd964135c0ea439d996d128
nh60211as <nh60211@gmail.com>
2025-01-03 11:21:31 +0800

    feat: add Day 3 R Row

--- src/data/Ff44GroupData.ts
+++ src/data/Ff44GroupData.ts
@@ -64,6 +64,7 @@ import { FF44_GROUP_DATA_DAY_3_ROW_N } from "./ff44/day3/Ff44GroupDataDay3RowN";
 import { FF44_GROUP_DATA_DAY_3_ROW_O } from "./ff44/day3/Ff44GroupDataDay3RowO";
 import { FF44_GROUP_DATA_DAY_3_ROW_P } from "./ff44/day3/Ff44GroupDataDay3RowP";
 import { FF44_GROUP_DATA_DAY_3_ROW_Q } from "./ff44/day3/Ff44GroupDataDay3RowQ";
+import { FF44_GROUP_DATA_DAY_3_ROW_R } from "./ff44/day3/Ff44GroupDataDay3RowR";
 
 export const FF44_GROUP_DATA: Array<GroupData> = (
   [] as Array<GroupData>
@@ -129,6 +130,7 @@ export const FF44_GROUP_DATA: Array<GroupData> = (
   FF44_GROUP_DATA_DAY_3_ROW_O,
   FF44_GROUP_DATA_DAY_3_ROW_P,
   FF44_GROUP_DATA_DAY_3_ROW_Q,
+  FF44_GROUP_DATA_DAY_3_ROW_R,
 );
 
 function getFf44BoothDataByActiveDay(
---
+++ src/data/ff44/day3/Ff44GroupDataDay3RowR.ts
@@ -0,0 +1,184 @@
+import { BoothActiveDay } from "../../../types/BoothActiveDay";
+import { GroupData } from "../../../types/GroupData";
+
+export const FF44_GROUP_DATA_DAY_3_ROW_R: Array<GroupData> = [
+  {
+    groupId: "cd1ed9a56476480594d331775c0931c1",
+    groupName: "MM´ Unit",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "R", number: 1 }],
+      },
+    ],
+  },
+  {
+    groupId: "5711cbbd75754db0bcfbeb6dc266ff1d",
+    groupName: "MOKOTANCHI",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "R", number: 2 }],
+      },
+    ],
+  },
+  {
+    groupId: "c8174113512341eaa2edb52f30f171ae",
+    groupName: "primore",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "R", number: 3 }],
+      },
+    ],
+  },
+  {
+    groupId: "5ecddacb1e6a417b93bd65df692b6ef8",
+    groupName: "Rua&Leeloo Cosplay",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "R", number: 4 }],
+      },
+    ],
+  },
+  {
+    groupId: "f20f9e42769f419ca7ceaddbf7da6cbf",
+    groupName: "兔兔蟲cosplay",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "R", number: 10 }],
+      },
+    ],
+  },
+  {
+    groupId: "3da51be2ca1c4516b5cfa960ce0db260",
+    groupName: "WHO　YUA",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "R", number: 11 }],
+      },
+    ],
+  },
+  {
+    groupId: "01d93fe49dde4f839c0bb63627185b4f",
+    groupName: "一隻小小",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "R", number: 12 }],
+      },
+    ],
+  },
+  {
+    groupId: "1dc8b70b14d246478a978478cd55dae4",
+    groupName: "小羊/풍신 Kohitsuji_Pungsin",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "R", number: 13 }],
+      },
+    ],
+  },
+  {
+    groupId: "009d7b5f126b48a89a594ed2d71faf00",
+    groupName: "｜SHIZURU ✤ 小紫｜",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "R", number: 14 }],
+      },
+    ],
+  },
+  {
+    groupId: "49df7a04eca443efb6c0f590a662c2ae",
+    groupName: "糖糖Candi",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "R", number: 15 }],
+      },
+    ],
+  },
+  {
+    groupId: "3c9025ec35a14dfe9a594c55d84d993a",
+    groupName: "Aki cosplay",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "R", number: 16 }],
+      },
+    ],
+  },
+  {
+    groupId: "56ac0bb0f5e74d1381b5bb40fc7f8ed4",
+    groupName: "Ruru茹茹",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "R", number: 17 }],
+      },
+    ],
+  },
+  {
+    groupId: "78e9b88073634ce9a79cb966060744c0",
+    groupName: "Sum sum Danka x 裁縫姬",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [
+          { row: "R", number: 19 },
+          { row: "R", number: 20 },
+        ],
+      },
+    ],
+  },
+  {
+    groupId: "b196fd9e25ac4e46b552a2dd08341d7f",
+    groupName: "貓草Cosplay",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "R", number: 23 }],
+      },
+    ],
+  },
+  {
+    groupId: "753d272668244edbbea6129c8d3565ad",
+    groupName: "ももあめ",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "R", number: 34 }],
+      },
+    ],
+  },
+  {
+    groupId: "839e9a34047b4bd086d9a81a0ed30771",
+    groupName: "あみったけ",
+    groupLink: null,
+    boothList: [
+      {
+        activeDay: BoothActiveDay.day3,
+        boothNumberList: [{ row: "R", number: 40 }],
+      },
+    ],
+  },
+];
