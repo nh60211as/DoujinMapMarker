e5c2566fdff8bf544e1964d095e550c9eb968ee1
nh60211as <nh60211@gmail.com>
2025-04-28 18:45:26 +0800

    feat: parametrize script

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,12 +1,30 @@
+import { EventType, eventTypeArray } from '../src/types/EventType';
 import { GroupData } from '../src/types/GroupData';
 import { csvToGroupDataList } from './script';
 import * as fs from 'fs';
 
+// FIXME: actual find the event type
+// current command `node ./generated-script/scripts/index.js IF6`
+const eventTypeNullableString: string | undefined = process.argv[2];
+
+if (eventTypeNullableString === undefined) {
+  console.error(`Expected input argument.`);
+}
+
+if (eventTypeArray.includes(eventTypeNullableString as EventType) === false) {
+  console.error(`Invalid input event type [${eventTypeNullableString}].`);
+}
+
+const eventType: EventType = eventTypeNullableString as EventType;
+
 // NOTE: The implementation should be changed with each event
-csvToGroupDataList('./data/IF6/group-list.csv', './data/IF6/booth-list.csv')
+csvToGroupDataList(
+  `./data/${eventType}/group-list.csv`,
+  `./data/${eventType}/booth-list.csv`,
+)
   .then((groupDataList: Array<GroupData>) => {
     fs.writeFileSync(
-      './generated-data/IF6/group-data.json',
+      `./generated-data/${eventType}/group-data.json`,
       JSON.stringify(groupDataList, null, 2), // 3rd argument is spacing level
       'utf-8',
     );
--- src/types/EventType.ts
+++ src/types/EventType.ts
@@ -1,4 +1,6 @@
-export type EventType = 'FF44' | 'CWTxACCF' | 'PF42' | 'IF6';
-
 // NOTE: The implementation should be changed with each event
+export const eventTypeArray = ['FF44', 'CWTxACCF', 'PF42', 'IF6'] as const;
+
+export type EventType = (typeof eventTypeArray)[number];
+
 export const CURRENT_EVENT_TYPE = 'IF6';
